2f8def595818158fdf1b49bd3a19b6160a54002e
Zoran <pujic.zoran@gmail.com>
2023-05-24 18:49:09 +0200

    finish styles and script

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const button = document.querySelector(".toggle-button");
 const paragraph = document.querySelector(".hidden-text");
+const rectangle = document.querySelector(".rectangle");
 
 button.addEventListener("click", function () {
   button.style.display = "none";
   paragraph.classList.add("visible");
+  rectangle.classList.add("visible");
 });
